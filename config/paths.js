a16eace5afa2b81da7fd478cfd3e567364f33eec
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2020-01-30 22:53:13 -0500

    (#46) Implemented api mocking for Term endpoint - Implements proxy - Updated path to find setupProxy in support folder to separate app code    from support. - Added a few examples - Added README notes

--- config/paths.js
+++ config/paths.js
@@ -79,7 +79,7 @@ module.exports = {
   appJsConfig: resolveApp('jsconfig.json'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
-  proxySetup: resolveApp('src/setupProxy.js'),
+  proxySetup: resolveApp('support/src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
