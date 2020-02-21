d5a90682622754fca2ea42d4db647375dfd1c9f1
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2020-02-21 15:57:58 -0500

    (#29) Media display on term definition page- create component for cgov image- create component for cgov video- unit tests and integration test addedupdates to integration tests for Definition for better coverage and missing spanish text for header

--- src/views/Definition/Definition.jsx
+++ src/views/Definition/Definition.jsx
@@ -1,38 +1,101 @@
-import React from 'react';
-import { useQuery } from 'react-fetching-library';
-import { useParams } from 'react-router';
+import React from "react";
+import { useQuery } from "react-fetching-library";
+import { useParams } from "react-router";
+import { useStateValue } from "../../store/store.js";
 
-import { Spinner } from '../../components';
-import { testIds } from '../../constants';
-import { getTermDefinition } from '../../services/api/actions';
+import { Spinner, FigureCgovImage, FigureCgovVideo } from "../../components";
+import { testIds } from "../../constants";
+import { getTermDefinition } from "../../services/api/actions";
 
 const Definition = () => {
   const { idOrName } = useParams();
-  const { loading, payload, error } = useQuery( getTermDefinition( idOrName ) );
+  const { loading, payload, error } = useQuery(getTermDefinition(idOrName));
+  const [{ language }] = useStateValue();
+
+  const renderRelatedResources = () => {
+    return (
+      <div className="related-resources">
+        {renderRelatedResourceLinks()}
+        {renderMediaItems()}
+      </div>
+    );
+  };
+
+  const renderRelatedResourceLinks = () => (<></>);
+
+  const renderMediaItems = () => {
+    return (
+      <>
+        {payload.media.length > 0 &&
+          payload.media.map(mediaItem => {
+            if (mediaItem.Type === "Image") {
+              const imgArr = mediaItem.ImageSources;
+              const thumbUri = imgArr.find(imgItem => imgItem.Size === "571")
+                .Src;
+              const enlargeUri = imgArr.find(
+                imgItem => imgItem.Size === "original"
+              ).Src;
+              return (
+                <FigureCgovImage
+                  altText={mediaItem.Alt}
+                  caption={mediaItem.Caption}
+                  classes="image-left-medium"
+                  key={mediaItem.Ref}
+                  lang={language}
+                  thumb_uri={thumbUri}
+                  enlarge_uri={enlargeUri}
+                />
+              );
+            } else if (mediaItem.Type === "Video") {
+              return (
+                <FigureCgovVideo
+                  classes="video center size75"
+                  key={mediaItem.UniqueId}
+                  videoId={mediaItem.UniqueId}
+                  videoTitle={mediaItem.Title}
+                >
+                  {mediaItem.Caption}
+                </FigureCgovVideo>
+              );
+            } else {
+              return false;
+            }
+          })}
+      </>
+    );
+  };
 
   return (
     <>
-      { loading &&
-        <Spinner />
-      }
-      { !loading && !error && payload &&
-        <div>
-            <h1 className="term-title"
-                data-testid={testIds.TERM_DEF_TITLE}
-                data-cdr-id={ payload.termId }
+      {loading && <Spinner />}
+      {!loading && !error && payload && (
+        <>
+          <h1
+            className="term-title"
+            data-testid={testIds.TERM_DEF_TITLE}
+            data-cdr-id={payload.termId}
+          >
+            {payload.termName}
+          </h1>
+          {payload.pronunciation && (
+            <div
+              className="pronunciation"
+              data-testid={testIds.TERM_DEF_PRONUNCIATION}
             >
-                { payload.termName }
-            </h1>
-            { payload.pronunciation &&
-            <div className="pronunciation" data-testid={testIds.TERM_DEF_PRONUNCIATION}>
-                {payload.pronunciation.key}
+              {payload.pronunciation.key}
             </div>
-            }
-            { payload.definition &&
-                <div data-testid={testIds.TERM_DEF_DESCRIPTION} dangerouslySetInnerHTML={{__html: payload.definition.html}}></div>
-            }
-        </div>
-      }
+          )}
+          {payload.definition && (
+            <div
+              data-testid={testIds.TERM_DEF_DESCRIPTION}
+              dangerouslySetInnerHTML={{ __html: payload.definition.html }}
+            ></div>
+          )}
+          {((payload.relatedResources && payload.relatedResources.length > 0) ||
+            (payload.media && payload.media.length > 0)) &&
+            renderRelatedResources()}
+        </>
+      )}
     </>
   );
 };
--- src/views/Definition/__tests__/Definition.en.test.js
+++ src/views/Definition/__tests__/Definition.en.test.js
@@ -1,14 +1,14 @@
-import { act, cleanup, render } from '@testing-library/react';
-import React from 'react';
-import { useParams } from 'react-router';
-import { ClientContextProvider } from 'react-fetching-library';
+import { act, cleanup, render } from "@testing-library/react";
+import React from "react";
+import { useParams } from "react-router";
+import { ClientContextProvider } from "react-fetching-library";
 
-import { testIds } from '../../../constants';
-import Definition from '../Definition';
-import { useStateValue } from '../../../store/store.js';
-import fixtures from '../../../utils/fixtures';
+import { testIds } from "../../../constants";
+import Definition from "../Definition";
+import { useStateValue } from "../../../store/store.js";
+import fixtures from "../../../utils/fixtures";
 
-jest.mock('react-router');
+jest.mock("react-router");
 jest.mock("../../../store/store.js");
 
 let client;
@@ -17,92 +17,174 @@ let wrapper;
 
 const { getFixture } = fixtures;
 const fixturePath = `/Terms/Cancer.gov/Patient`;
-const hpvFile = '44386__hpv.json';
-const metastaticFile = '44058__metastatic.json';
+const hpvFile = "44386__hpv.json";
+const metastaticFile = "44058__metastatic.json";
 
-describe('Definition component with English', () => {
-    const idOrPurl = 'metastatic';
-    const language = 'en';
+const unsupportedMediaFile = {
+  termId: 1337,
+  language: "en",
+  dictionary: "Cancer.gov",
+  audience: "Patient",
+  termName: "Unsupported Media Test",
+  firstLetter: "h",
+  prettyUrlName: "hpv",
+  pronunciation: null,
+  definition: {
+    html:
+      "A type of virus that can cause abnormal tissue growth (for example, warts) and other changes to cells. Infection for a long time with certain types of HPV can cause cervical cancer. HPV may also play a role in some other types of cancer, such as anal, vaginal, vulvar, penile, and oropharyngeal cancers.  Also called human papillomavirus.",
+    text:
+      "A type of virus that can cause abnormal tissue growth (for example, warts) and other changes to cells. Infection for a long time with certain types of HPV can cause cervical cancer. HPV may also play a role in some other types of cancer, such as anal, vaginal, vulvar, penile, and oropharyngeal cancers.  Also called human papillomavirus."
+  },
+  relatedResources: [],
+  media: [
+    {
+      Type: "Zip File",
+      Ref: "CDR0000764135",
+      Alt:
+        "Metastasis; drawing shows primary cancer that has spread from the colon to other parts of the body (the liver and the lung). An inset shows cancer cells spreading from the primary cancer, through the blood and lymph system, to another part of the body where a metastatic tumor has formed.",
+      Caption:
+        "Metastasis. In metastasis, cancer cells break away from where they first formed (primary cancer), travel through the blood or lymph system, and form new tumors (metastatic tumors) in other parts of the body. The metastatic tumor is the same type of cancer as the primary tumor. "
+    }
+  ]
+};
 
-    definition = getFixture(`${fixturePath}/${language}/${metastaticFile}`);
+describe("Definition component with English", () => {
+  const idOrPurl = "metastatic";
+  const language = "en";
 
-    client = {
+  definition = getFixture(`${fixturePath}/${language}/${metastaticFile}`);
+
+  client = {
+    query: async () => ({
+      error: false,
+      status: 200,
+      payload: definition
+    })
+  };
+
+  beforeEach(async () => {
+    const dictionaryName = "Cancer.gov";
+    const dictionaryTitle = "NCI Dictionary of Cancer Terms";
+    useParams.mockReturnValue({
+      idOrName: idOrPurl
+    });
+    useStateValue.mockReturnValue([
+      {
+        appId: "mockAppId",
+        basePath: "/",
+        dictionaryName,
+        dictionaryTitle,
+        language
+      }
+    ]);
+
+    await act(async () => {
+      wrapper = render(
+        <ClientContextProvider client={client}>
+          <Definition />
+        </ClientContextProvider>
+      );
+    });
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("Match Term Name for Definition", () => {
+    const { getByText } = wrapper;
+    expect(getByText(idOrPurl)).toBeTruthy();
+  });
+
+  test('Ensure attribute "data-cdr-id" exists with correct value for Definition component', () => {
+    const { getByTestId } = wrapper;
+    expect(getByTestId(testIds.TERM_DEF_TITLE)).toHaveAttribute(
+      "data-cdr-id",
+      `${definition.termId}`
+    );
+  });
+
+  // Media related tests
+  test("Images specified in media are displayed", () => {
+    const { container } = wrapper;
+    expect(
+      container.querySelectorAll("figure.image-left-medium").length
+    ).toEqual(1);
+  });
+
+  test("Video specified in media are displayed", () => {
+    const { container } = wrapper;
+    expect(container.querySelectorAll("figure.video").length).toEqual(1);
+  });
+
+  describe("Unsupported media types", () => {
+    beforeEach(cleanup);
+    test("do not yield output", async () => {
+      definition = unsupportedMediaFile;
+
+      const client = {
         query: async () => ({
-            error: false,
-            status: 200,
-            payload: definition
-        }),
-    };
+          error: false,
+          status: 200,
+          payload: definition
+        })
+      };
 
-    beforeEach( async () => {
-        const dictionaryName = 'Cancer.gov';
-        const dictionaryTitle = 'NCI Dictionary of Cancer Terms';
-        useParams.mockReturnValue({
-            idOrName: idOrPurl
-        });
-        useStateValue.mockReturnValue([
-          {
-            appId: "mockAppId",
-            basePath: '/',
-            dictionaryName,
-            dictionaryTitle,
-            language
-          }
-        ]);
+      useParams.mockReturnValue({
+        idOrName: unsupportedMediaFile
+      });
 
-        await act( async () => {
-            wrapper = render(
-                <ClientContextProvider client={client}>
-                    <Definition/>
-                </ClientContextProvider>
-            );
-        });
+      await act(async () => {
+        wrapper = render(
+          <ClientContextProvider client={client}>
+            <Definition />
+          </ClientContextProvider>
+        );
+      });
+      const { container } = wrapper;
+
+      expect(container.querySelectorAll("figure.video").length).toEqual(0);
+      expect(
+        container.querySelectorAll("figure.image-left-medium").length
+      ).toEqual(0);
+    });
+  });
+
+  describe('Displaying the information for the term "hpv"', () => {
+    const idOrPurl = "hpv";
+    beforeEach(cleanup);
+    test("Ensure pronunciation is not displayed for a definition without one", async () => {
+      definition = getFixture(`${fixturePath}/${language}/${hpvFile}`);
+
+      const client = {
+        query: async () => ({
+          error: false,
+          status: 200,
+          payload: definition
+        })
+      };
+
+      useParams.mockReturnValue({
+        idOrName: idOrPurl
+      });
+
+      await act(async () => {
+        wrapper = render(
+          <ClientContextProvider client={client}>
+            <Definition />
+          </ClientContextProvider>
+        );
+      });
+      const { queryByTestId } = wrapper;
+      expect(queryByTestId(testIds.TERM_DEF_PRONUNCIATION)).toBeNull();
     });
 
-    afterEach(() => {
-        cleanup();
+    test("No media should be displayed when empty", () => {
+      const { container } = wrapper;
+      expect(container.querySelectorAll("figure.video").length).toEqual(0);
+      expect(
+        container.querySelectorAll("figure.image-left-medium").length
+      ).toEqual(0);
     });
-
-    test('Match Term Name for Definition', () => {
-        const { getByText } = wrapper;
-        expect(getByText(idOrPurl)).toBeTruthy();
-    });
-
-    test('Ensure attribute "data-cdr-id" exists with correct value for Definition component', () => {
-        const { getByTestId } = wrapper;
-        expect(getByTestId(testIds.TERM_DEF_TITLE)).toHaveAttribute('data-cdr-id', `${definition.termId}`);
-    });
-
-    describe('', () => {
-        const idOrPurl = 'hpv';
-        beforeEach(cleanup);
-        test('Ensure pronunciation is not displayed for a definition without one', async () => {
-            definition = getFixture(`${fixturePath}/${language}/${hpvFile}`);
-
-            const client = {
-                query: async () => ({
-                    error: false,
-                    status: 200,
-                    payload: definition
-                }),
-            };
-
-            useParams.mockReturnValue({
-                idOrName: idOrPurl
-            });
-
-            await act( async () => {
-                wrapper = render(
-                    <ClientContextProvider client={client}>
-                        <Definition/>
-                    </ClientContextProvider>
-                );
-            });
-            const { queryByTestId } = wrapper;
-            expect(queryByTestId(testIds.TERM_DEF_PRONUNCIATION)).toBeNull();
-        });
-    });
+  });
 });
-
-
-
