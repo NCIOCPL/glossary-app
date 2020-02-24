f0578f7846060004c7c1b4920ec2f8e6e965c722
Seyi Longe <oluwaseun.longe@nih.gov>
2020-02-23 20:05:28 -0500

    (#76) Added Dictionary Search and A-Z List component
    Closes #76
    Closes #78
    
    Increased test coverage
    
    Removed unwanted file
    
    Put back missing required prop removed in error
    
    Removed Search component from Home, moved to SearchBox
    
    Removed route for search
    
    Added Spanish language support
    
    Organized components into dirs, style import updates, added searchBoxTitle to initialize
    
    Added newline to eof
    
    Added newline to eof for missed file
    
    (#76) Added AZList component
    
    Closes #79
    
    Added Spanish language support
    
    Added expand route
    
    Organized atomic elements into dirs, swapped div for nav
    
    Updated styling and dom
    
    Moved ul style declaration to nci common and removed AZList.scss
    
    Fixed file naming
    
    Directory restructure update
    
    Reverted changes in nci common, add styling to AZList.scss
    
    Sassified declarations
    
    Added newline to eof

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 // Test Ids
 export const testIds = {
+  AZ_LIST: 'tid-az-list',
+  SEARCH_CONTAINER: 'dictionary-search',
   TERM_DEF_DESCRIPTION: 'term-def-description',
   TERM_DEF_PRONUNCIATION: 'term-def-pronunciation',
   TERM_DEF_TITLE: 'term-def-title'
---
+++ src/utils/i18n.js
@@ -0,0 +1,18 @@
+export const i18n = {
+    browse: {
+        en: 'Browse',
+        es: 'Ojear'
+    },
+    contains: {
+        en: 'Contains',
+        es: 'Contiene'
+    },
+    search: {
+        en: 'Search',
+        es: 'Buscar'
+    },
+    startsWith: {
+        en: 'Starts with',
+        es: 'Empieza con'
+    }
+};
