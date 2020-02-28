41b5478a75c93ab558e80f1deed671dc00a2c70d
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2020-02-28 15:26:59 -0500

    (#25) related resource links- implemented list for related resources- unit and integration tests added(#25)Added tests for More Information resources
    - changed  glossary resource to use termId
       if prettyUrlName is not
       available
    - also updated unit tests
    - updated feature file

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
 //atoms
 export { default as AudioPlayer } from "./atomic/audio-player";
+export { default as AZList } from "./atomic/az-list";
+export { default as InputLabel } from "./atomic/InputLabel";
+export { default as Radio } from "./atomic/Radio";
 export { default as Spinner } from "./atomic/spinner";
+export { default as TextInput } from "./atomic/TextInput";
 export { default as YoutubeVideoPlayer } from "./atomic/youtube-video-player";
-export { default as SearchBox } from "./molecules/search-box";
 
 // molecules
 export { FigureCgovImage, FigureCgovVideo } from "./molecules/figures";
 export { default as Pronunciation } from "./molecules/pronunciation";
+export { default as RelatedResourceList } from "./molecules/related-resource-list";
+export { default as Search } from "./molecules/search-box";
+export { default as SearchBox } from "./molecules/search-box";
