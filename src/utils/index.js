c40d53d739cdd108c1767595dcdba5ea0012f382
Kate Mashkina <kate-mashkina@users.noreply.github.com>
2020-03-04 17:24:34 -0500

    (#97) term count placeholder: - added new endpoint - new setupProxy method - mock data - added an util to format any number to be displayed with comma separated thousands - added unit tests - added token parser util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
 export { default as fixtures } from "./fixtures";
 export { i18n } from "./i18n";
 export { getProductTestBase } from "./getProductTestBase";
+export { formatNumberToThousands } from "./number"
+export { TokenParser } from "./replaceTokens"
+
