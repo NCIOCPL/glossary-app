7e4c6adf4495cdd1d7e8563e961e03873a746e5c
Sarina <sarinapadilla@users.noreply.github.com>
2020-05-27 12:51:10 -0400

    (#26) Single search result redirects to definition
    * Update TermList to redirect to Definition if search returns one term
    * Add unit tests
    * Add integration tests and mock data
    * Closes #26

--- src/views/Terms/__tests__/TermList.test.js
+++ src/views/Terms/__tests__/TermList.test.js
@@ -1,7 +1,7 @@
 import { act, cleanup, render } from "@testing-library/react";
 import React from 'react';
 import { ClientContextProvider } from "react-fetching-library";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, useLocation, useNavigate } from "react-router-dom";
 
 import { testIds } from "../../../constants";
 import { TermList } from "../index";
@@ -11,6 +11,7 @@ import fixtures from "../../../utils/fixtures";
 jest.mock("../../../store/store");
 
 let client;
+let location;
 let termList;
 let wrapper;
 
@@ -18,6 +19,11 @@ const query = "A";
 const queryFile = `${query}.json`;
 const { getFixture } = fixtures;
 const fixturePath = `/Terms/expand/Cancer.gov/Patient`;
+
+function ComponentWithLocation({ RenderComponent, query }) {
+    location = useLocation();
+    return <RenderComponent query={query} />;
+}
 
 describe('TermList component rendered with English', () => {
     const language = "en";
@@ -63,7 +69,6 @@ describe('TermList component rendered with English', () => {
     afterEach(() => {
         cleanup();
     });
-
     test(`"${termListCount} results found for: ${query}" should be present `, () => {
         const { getByText } = wrapper;
         expect(getByText(`${termListCount} results found for: ${query}`)).toBeInTheDocument();
@@ -90,6 +95,18 @@ describe('TermList component rendered with English', () => {
                     }
                 })
             };
+            const dictionaryName = "Cancer.gov";
+            const dictionaryTitle = "NCI Dictionary of Cancer Terms";
+            useStateValue.mockReturnValue([
+                {
+                    appId: "mockAppId",
+                    basePath: "/",
+                    dictionaryName,
+                    dictionaryTitle,
+                    language,
+                    searchBoxTitle
+                }
+            ]);
 
             await act(async () => {
                 wrapper = render(
@@ -119,5 +136,130 @@ describe('TermList component rendered with English', () => {
             expect(window.scrollTo).toHaveBeenCalledTimes(1);
             expect(window.scrollTo).toHaveBeenLastCalledWith(0, 0);
         });
+    });
+
+    describe(`Tests using lung cancer as query parameter for term`, () => {
+        beforeEach(cleanup);
+        afterEach(cleanup);
+        const query = "lung cancer";
+
+        test(`Redirected to definition page with pretty URL name for query "${query}" with one term result`, async () => {
+
+            const client = {
+                query: async () => ({
+                    error: false,
+                    status: 200,
+                    payload: {
+                        meta: {
+                            totalResults: 1,
+                            from: 0
+                        },
+                        results: [
+                            {
+                                termId: 445043,
+                                language: "en",
+                                dictionary: "Cancer.gov",
+                                audience: "Patient",
+                                termName: "lung cancer",
+                                firstLetter: "l",
+                                prettyUrlName: "lung-cancer",
+                                pronunciation: {
+                                    key: "(lung KAN-ser)",
+                                    audio: "https://nci-media.cancer.gov/pdq/media/audio/714622.mp3"
+                                },
+                                definition: {
+                                    html: "Cancer that forms in tissues of the lung, usually in the cells lining air passages. The two main types are small cell lung cancer and non-small cell lung cancer. These types are diagnosed based on how the cells look under a microscope.",
+                                    text: "Cancer that forms in tissues of the lung, usually in the cells lining air passages. The two main types are small cell lung cancer and non-small cell lung cancer. These types are diagnosed based on how the cells look under a microscope."
+                                },
+                                otherLanguages: [],
+                                relatedResources: [],
+                                media: []
+                            }
+                        ],
+                        links: null
+                    },
+                    loading: false
+                })
+            };
+
+            const expectedLocationObject = {
+                pathname: '/def/lung-cancer',
+                search: '',
+                hash: '',
+                state: null,
+                key: expect.any(String)
+            };
+
+            await act( async () => {
+                wrapper = await render(
+                    <MemoryRouter initialEntries={['/']}>
+                        <ClientContextProvider client={client}>
+                            <ComponentWithLocation RenderComponent={TermList} />
+                        </ClientContextProvider>
+                    </MemoryRouter>
+                );
+            });
+            expect(location).toMatchObject(expectedLocationObject);
+        });
+
+        test(`Redirected to definition page with term ID for query "${query}" with one term result`, async () => {
+
+            const client = {
+                query: async () => ({
+                    error: false,
+                    status: 200,
+                    payload: {
+                        meta: {
+                            totalResults: 1,
+                            from: 0
+                        },
+                        results: [
+                            {
+                                termId: 445043,
+                                language: "en",
+                                dictionary: "Cancer.gov",
+                                audience: "Patient",
+                                termName: "lung cancer",
+                                firstLetter: "l",
+                                prettyUrlName: null,
+                                pronunciation: {
+                                    key: "(lung KAN-ser)",
+                                    audio: "https://nci-media.cancer.gov/pdq/media/audio/714622.mp3"
+                                },
+                                definition: {
+                                    html: "Cancer that forms in tissues of the lung, usually in the cells lining air passages. The two main types are small cell lung cancer and non-small cell lung cancer. These types are diagnosed based on how the cells look under a microscope.",
+                                    text: "Cancer that forms in tissues of the lung, usually in the cells lining air passages. The two main types are small cell lung cancer and non-small cell lung cancer. These types are diagnosed based on how the cells look under a microscope."
+                                },
+                                otherLanguages: [],
+                                relatedResources: [],
+                                media: []
+                            }
+                        ],
+                        links: null
+                    },
+                    loading: false
+                })
+            };
+
+            const expectedLocationObject = {
+                pathname: '/def/445043',
+                search: '',
+                hash: '',
+                state: null,
+                key: expect.any(String)
+            };
+
+            await act( async () => {
+                wrapper = await render(
+                    <MemoryRouter initialEntries={['/']}>
+                        <ClientContextProvider client={client}>
+                            <ComponentWithLocation RenderComponent={TermList} />
+                        </ClientContextProvider>
+                    </MemoryRouter>
+                );
+            });
+            expect(location).toMatchObject(expectedLocationObject);
+        });
+
     });
 });
